df3bdc2c14bde0be981a15cdfdbcc057b12d3700
Enzo Arata <enzoarata50@nevada.unr.edu>
2022-01-31 11:35:06 -0800

    Added workout scaffolding and active record association

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,6 @@
 class Group < ApplicationRecord
   has_and_belongs_to_many :users
+  has_many: :workouts
 
   validates :title, presence: true,
     uniqueness: { case_sensitive: false },
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_17_200510) do
+ActiveRecord::Schema.define(version: 2022_01_31_192126) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -48,6 +48,15 @@ ActiveRecord::Schema.define(version: 2022_01_17_200510) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.boolean "admin", default: false
+  end
+
+  create_table "workouts", force: :cascade do |t|
+    t.string "title"
+    t.integer "components"
+    t.string "type"
+    t.integer "group_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
